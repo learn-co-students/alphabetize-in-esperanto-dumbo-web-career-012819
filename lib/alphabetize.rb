2f9c2d7e7d2afa378311329858d933fa27be02e2
Angelo Spampinato <spampinatodesign@gmail.com>
2019-01-31 10:33:55 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+	arr.sort_by {|sentence| sentence.split("").map{|letter| ESPERANTO_ALPHABET.index(letter)}}
 end
